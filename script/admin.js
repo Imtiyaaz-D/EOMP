4e2d938adaa063c5105640de32ad1b946364c5b4
Imtiyaaz-D <imtdamon777@gmail.com>
2023-06-12 14:49:09 +0200

    admin

---
+++ script/admin.js
@@ -0,0 +1,36 @@
+// debugger
+let table = document.querySelector(".box")
+let products = [{
+    id : "1",
+    name:"Converse-Black",
+    price : 600,
+    image : "https://i.postimg.cc/Ghg2F0DS/images-shoe-all-star.jpg",
+    Description : "A pair of black Converse tekkies",
+},
+{   id : "2",
+    name:"Converse-White",
+    price : 750,
+    image : "https://i.postimg.cc/mDPVrWQf/istockphoto-175537625-612x612-convserve-shoe-3.jpg",
+    Description : "A pair of white Converse tekkies",
+},
+{
+    id : "3",
+    name:"Vans",
+    price : 350,
+    image : "https://i.postimg.cc/nhHhWGKQ/images-vans-shoe-2.jpg",
+    Description : "A pair of vans tekkies",
+},
+]
+console.log(products)
+products.forEach(item =>{
+    table.innerHTML +=
+    `<tr>
+    <td>${item.id}</td>
+    <td>${item.name}</td>
+    <td>${item.price}</td>
+    <td><img src="${item.image}" alt="${item.name}"></td>
+    <td>${item.Description}</td>
+    <td><button>add</button></td>
+    <td><button>delete</button></td>
+  </tr>`
+})
