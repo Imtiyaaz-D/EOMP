86317fbf0d55fd2485fd9aab6f9800ebecab2ad6
Imtiyaaz-D <imtdamon777@gmail.com>
2023-06-13 14:23:09 +0200

    editing

--- script/admin.js
+++ script/admin.js
@@ -1,24 +1,25 @@
 // debugger
 let table = document.querySelector(".box")
+const addBtn = document.querySelector('#add-btn')
 let products = [{
     id : "1",
     name:"Converse-Black",
     price : 600,
     image : "https://i.postimg.cc/Ghg2F0DS/images-shoe-all-star.jpg",
-    Description : "A pair of black Converse tekkies",
+    Description : "A pair of black Converse takkies",
 },
 {   id : "2",
     name:"Converse-White",
     price : 750,
     image : "https://i.postimg.cc/mDPVrWQf/istockphoto-175537625-612x612-convserve-shoe-3.jpg",
-    Description : "A pair of white Converse tekkies",
+    Description : "A pair of white Converse takkies",
 },
 {
     id : "3",
     name:"Vans",
     price : 350,
     image : "https://i.postimg.cc/nhHhWGKQ/images-vans-shoe-2.jpg",
-    Description : "A pair of vans tekkies",
+    Description : "A pair of vans takkies",
 },
 ]
 console.log(products)
@@ -27,10 +28,52 @@ products.forEach(item =>{
     `<tr>
     <td>${item.id}</td>
     <td>${item.name}</td>
-    <td>${item.price}</td>
+    <td>R${item.price}</td>
     <td><img src="${item.image}" alt="${item.name}"></td>
     <td>${item.Description}</td>
     <td><button>add</button></td>
-    <td><button>delete</button></td>
-  </tr>`
+    <td><button>edit</button></td>
+  </tr>
+  `
 })
+
+addBtn.addEventListener('click',addItem)
+
+// function addItem(e){
+//     event.preventDefault();
+//     if{ data.}
+// }
+
+
+
+
+// let btn = document.querySelector('.btn')
+// const data = JSON.parse(localStorage.getItem('data'))?JSON.parse(localStorage.getItem('data')) : [{
+//     id : "1",
+//     name:"Converse-Black",
+//     price : 600,
+//     image : "https://i.postimg.cc/Ghg2F0DS/images-shoe-all-star.jpg",
+//     Description : "A pair of black Converse tekkies",
+// },
+// {   id : "2",
+//     name:"Converse-White",
+//     price : 750,
+//     image : "https://i.postimg.cc/mDPVrWQf/istockphoto-175537625-612x612-convserve-shoe-3.jpg",
+//     Description : "A pair of white Converse tekkies",
+// },
+// {
+//     id : "3",
+//     name:"Vans",
+//     price : 350,
+//     image : "https://i.postimg.cc/nhHhWGKQ/images-vans-shoe-2.jpg",
+//     Description : "A pair of vans tekkies",
+// },
+// ]     
+// console.log(data)
+// const items = JSON.stringify(localStorage.setItem('data'))
+// add btn
+// const addBtn = document.querySelector('.btp')
+// addBtn.addEventListener('click',addItem)
+// function addBtn() {
+    
+// }
--- script/code.js
+++ script/code.js
@@ -1,19 +1,15 @@
-// const carousel = document.querySelector("carousel")
-
-let tabl = document.querySelector('table-cont')
-
-// let products = [];
-let shoe = [{
+const hc = document.querySelector('.hc')
+let products = [{
     id : "1",
     name:"Converse-Black",
     price : 600,
-    image : "https://i.postimg.cc/28DN5jKQ/images-shoe-all-star.jpg",
+    image : "https://i.postimg.cc/Ghg2F0DS/images-shoe-all-star.jpg",
     Description : "A pair of black Converse tekkies",
 },
 {   id : "2",
     name:"Converse-White",
     price : 750,
-    image : "https://i.postimg.cc/XJWF1CvH/istockphoto-175537625-612x612-convserve-shoe-3.jpg",
+    image : "https://i.postimg.cc/mDPVrWQf/istockphoto-175537625-612x612-convserve-shoe-3.jpg",
     Description : "A pair of white Converse tekkies",
 },
 {
@@ -24,15 +20,3 @@ let shoe = [{
     Description : "A pair of vans tekkies",
 },
 ]
-console.log(shoe)
-shoe.forEach(data =>{
-    tabl.innerHTML +=
-    `<tr>
-    <th>${item.id}</th>
-    <th>${item.name}</th>
-    <th><img src="${item.image}" alt="${item.name}"></th>
-    <th>${item.Description}</th>
-    <th><button>add</button></th>
-    <th><button>delete</button></th>
-  </tr>`
-})
